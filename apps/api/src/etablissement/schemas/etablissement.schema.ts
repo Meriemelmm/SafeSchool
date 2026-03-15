d17facbe13cc291e4707a3e7c90d52a307232ebd
meriem el mecaniqui <melmecaniqui@gmail.com>
2026-03-15 23:07:08 +0000

    shemas etabliseement

--- apps/api/src/etablissement/schemas/etablissement.schema.ts
+++ apps/api/src/etablissement/schemas/etablissement.schema.ts
@@ -1,23 +1,30 @@
-
 import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
 import { HydratedDocument } from 'mongoose';
-import {UserRole } from '@shared/index';
 
 export type EtablissementDocument = HydratedDocument<Etablissement>;
 
-
-
 @Schema({ timestamps: true })
 export class Etablissement {
- 
- 
-    
+  @Prop({ required: true, unique: true })
+  name: string;
+
+  @Prop({ required: true })
+  address: string;
+
+  @Prop({ required: true })
+  phone: string;
+
+  @Prop({ required: true, unique: true })
+  email: string;
+
+  @Prop({ required: false })
+  type?: string;
+
   @Prop({ default: false })
   isDeleted: boolean;
 
   @Prop({ type: Date, default: null })
   deletedAt: Date | null;
-  
 }
 
 export const EtablissementSchema = SchemaFactory.createForClass(Etablissement);
