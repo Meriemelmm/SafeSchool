4a563fad358f624d5ff54030ce0ad1dd791671c8
meriem el mecaniqui <melmecaniqui@gmail.com>
2026-03-23 16:32:47 +0100

    refactor: update mail module and shared enums

--- packages/shared/enums/index.ts
+++ packages/shared/enums/index.ts
@@ -1,1 +1,5 @@
 export * from './role.enum';
+export* from './signalement-enums';
+export *from './preuve-type.enum';
+export *from './roleIncedent.enum';
+export * from './etablissement-type.enum';
