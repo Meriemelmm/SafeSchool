4f8292ae5041494f40a655c7257dbdc5a3518c49
meriem el mecaniqui <melmecaniqui@gmail.com>
2026-03-30 20:18:04 +0100

    feat: add ServeStaticModule for serving static files from uploads directory

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -2,24 +2,32 @@ import { NestFactory } from '@nestjs/core';
 import { ValidationPipe } from '@nestjs/common';
 import { AppModule } from './app.module';
 import cookieParser from 'cookie-parser';
+import { join } from 'path';
+import { NestExpressApplication } from '@nestjs/platform-express';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
 
-  // Enable CORS with credentials
   app.enableCors({
     origin: process.env.FRONTEND_URL || 'http://localhost:3000',
     credentials: true,
   });
 
-  // Enable cookie parser
+  // app.useStaticAssets(join(__dirname, '..', '..', '..', 'uploads'), {
+  //   prefix: '/uploads/',
+  // });
+
+
+  console.log(join(process.cwd(), 'apps/api/uploads'));
+
   app.use(cookieParser());
 
-  // Activating global validation pipes (Best Practice)
-  app.useGlobalPipes(new ValidationPipe({
-    whitelist: true, // Strips out properties that don't have decorators
-    transform: true, // Automatically parse primitive types
-  }));
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      transform: true,
+    }),
+  );
 
   await app.listen(process.env.PORT ?? 3007);
 }
